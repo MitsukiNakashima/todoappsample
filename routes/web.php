b02c2cc278f737ca8b3d8d0cdf844ab51329efc6
Mitsuki Nakashima <mitsuki@usernoMacBook-puro.local>
2021-08-29 19:19:59 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::group(['middleware' => 'auth'], function() {
+  Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
+  Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+  Route::post('/folders/create', 'FolderController@create');
+  Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+  Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
+  Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+  Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
+  Route::get('/', 'HomeController@index')->name('index');
+});
+Auth::routes();
+
